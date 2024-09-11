d7c1309326fba5408dd27474414ef1452a9d9eb5
Katon Bagaskoro <bagaskoro55@gmail.com>
2024-09-11 15:00:15 +0700

    whateverivedoneididitforlove

--- Telyu Event Frontend/src/Pages/Login.jsx
+++ Telyu Event Frontend/src/Pages/Login.jsx
@@ -1,25 +1,67 @@
 import './Login.css'
-import loginbgimg from '../assets/loginBg-Placeholder.jpg'
+import loginbgimg from '../assets/LoginBg.png'
+import logoTevent from '../assets/TeventLogo.png'
+import mataCoret from '../assets/EyeSlashFill.png'
+import React, { useState } from 'react'
 const Login = () =>{
+    // state untuk password keliatan apa kga
+    const [passVisible, setPassVisible] = useState(false);
+    const togglePassVis =()=>{
+        setPassVisible(!passVisible)
+    }
 
+    // state untuk isi input
+    const [userEmail, setUserEmail] =useState('')
+    const [userPass, setUserPass] = useState('')
+
+    // state untuk handling isi input
+    const handleInputEmail = (event) =>{
+        setUserEmail(event.target.value)
+    }
+    const handleInputPassword = (event) =>{
+        setUserPass(event.target.value)
+    }
+
+    // masuk login pak eko
+    const handleMasukButton =() =>{
+
+    }
 
     return(
         <div className="loginBg" style={{backgroundImage:`url(${loginbgimg})`}}>
+            <div className='logo'>
+                <img src={logoTevent} alt="" />
+            </div>
             <div className="loginMasuk" style={{backgroundColor:'white'}}>
                 <div className='kotakLogin' style={{display:'flex', flexDirection:'column'}}>
                     <h1>Masuk</h1>
 
                     <input type="email" 
                     placeholder='Email'
+                    value={userEmail}
+                    onChange={handleInputEmail}
                     />
-                    <input type="password" 
-                    placeholder='Kata sandi'
-                    />
-                    <button className='MasukButton'>
+                    <div className='passwordInput' style={{display:'flex'}}>
+                        <input type={passVisible ? 'text' : 'password'}
+                        placeholder='Kata sandi'
+                        value={userPass}
+                        onChange={handleInputPassword}
+                        />
+                        <button className='showHide'
+                        onClick={togglePassVis}>
+                            <img 
+                            src={mataCoret} alt="sembunyikan/tampilkan" />
+                            </button>
+                    </div>
+                    <button 
+                    className='masukButton'
+                    onClick={handleMasukButton}>
                         Masuk
                     </button>
+
                 </div>
             </div>
+            
         </div>
     )
 }
